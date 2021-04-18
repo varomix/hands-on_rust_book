0fbbce88ea98dce3bfb4e92de62e4cf74c72adfa
varomix wey <varomix@gmail.com>
2021-04-17 21:48:06 -0600

    Book page 166

--- src/components.rs
+++ src/components.rs
@@ -21,6 +21,13 @@ pub struct WantsToMove {
     pub destination: Point,
 }
 
+
+#[derive(Clone, Copy, Debug, PartialEq)]
+pub struct WantsToAttack {
+    pub attacker: Entity,
+    pub victim: Entity,
+}
+
 #[derive(Clone, Copy, Debug, PartialEq)]
 pub struct Health {
     pub current: i32,
