87a7138bd4c0a8231c2736124953c99e795f0612
varomix wey <varomix@gmail.com>
2021-04-17 22:10:18 -0600

    Chapter 8 done

--- src/systems/player_input.rs
+++ src/systems/player_input.rs
@@ -3,6 +3,8 @@ use crate::prelude::*;
 #[system]
 #[read_component(Point)]
 #[read_component(Player)]
+#[read_component(Enemy)]
+#[write_component(Health)]
 pub fn player_input(
     ecs: &mut SubWorld,
     commands: &mut CommandBuffer,
@@ -25,6 +27,7 @@ pub fn player_input(
             .find_map(|(entity, pos)| Some((*entity, *pos + delta)))
             .unwrap();
         let mut enemies = <(Entity, &Point)>::query().filter(component::<Enemy>());
+        let mut did_something = false;
         if delta.x != 0 || delta.y != 0 {
             let mut hit_something = false;
             enemies
@@ -34,6 +37,7 @@ pub fn player_input(
                 })
                 .for_each(|(entity, _)| {
                     hit_something = true;
+                    did_something = true;
 
                     commands
                         .push(((), WantsToAttack {
@@ -42,11 +46,20 @@ pub fn player_input(
                         }));
                 });
             if !hit_something {
+                did_something = true;
                 commands
                     .push(((), WantsToMove {
                         entity: player_entity,
                         destination,
                     }));
+            }
+        }
+        if !did_something {
+            if let Ok(mut health) = ecs
+                .entry_mut(player_entity)
+                .unwrap()
+                .get_component_mut::<Health>() {
+                health.current = i32::min(health.max, health.current + 1);
             }
         }
         *turn_state = TurnState::PlayerTurn;
